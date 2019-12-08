17a508cb474c2ee71fec05d5ca3ad1a772f3be72
DESKTOP-7U407JF\User <arminas.pamakstis@ktu.edu>
2019-12-08 17:19:59 +0200

    updated nugets

---
+++ Logic/Users/Models/JwtSettings.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Users.Models
+{
+    [JsonObject("tokenManagement")]
+    class JwtSettings
+    {
+        [JsonProperty("secret")]
+        public string Secret { get; set; }
+
+        [JsonProperty("issuer")]
+        public string Issuer { get; set; }
+
+        [JsonProperty("audience")]
+        public string Audience { get; set; }
+
+        [JsonProperty("accessExpiration")]
+        public int AccessExpiration { get; set; }
+
+        [JsonProperty("refreshExpiration")]
+        public int RefreshExpiration { get; set; }
+    }
+}
